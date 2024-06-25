35a2b5b88b173f57a4302e986dca8c3fdb1c817c
Himaja <madalahimaja21331@gmail.com>
2024-06-25 17:24:52 -0500

    cartpage

--- src/app/components/pages/cart-page/cart-page.component.ts
+++ src/app/components/pages/cart-page/cart-page.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html',
-  styleUrl: './cart-page.component.css'
+  styleUrls: ['./cart-page.component.css']
 })
-export class CartPageComponent {
+export class CartPageComponent implements OnInit {
+  cart: any[] = [];  // Replace this with your actual cart data structure
 
+  constructor() { }
+
+  ngOnInit(): void {
+    // Load cart data from a service or local storage
+    this.loadCart();
+  }
+
+  loadCart() {
+    // This is where you would load your cart data, for example:
+    // this.cart = this.cartService.getCartItems();
+    // For this example, we'll assume the cart is empty
+    this.cart = [];
+  }
+
+  isCartEmpty(): boolean {
+    return this.cart.length === 0;
+  }
 }
